641f44a2997ee8c7a796c783e73457220bf8e749
RussiaPlayer <wiskystar2@gmail.com>
2023-01-26 16:11:00 +0100

    Add Play command to message

--- src/main/java/com/github/russiaplayer/bot/CommandEventListener.java
+++ src/main/java/com/github/russiaplayer/bot/CommandEventListener.java
@@ -1,17 +1,28 @@
 package com.github.russiaplayer.bot;
 
+import com.github.russiaplayer.Entity.Server;
 import com.github.russiaplayer.SQL.ServerRepo;
 import com.github.russiaplayer.commands.Command;
 import com.github.russiaplayer.commands.CommandRegistry;
+import com.github.russiaplayer.commands.PlayCommand;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
-import net.dv8tion.jda.api.events.message.GenericMessageEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import static com.github.russiaplayer.bot.MessageSender.getMessageData;
+import static com.github.russiaplayer.bot.MessageSender.sendMessageToMusicChannel;
 
 public class CommandEventListener extends ListenerAdapter {
     private static final Logger LOG = LoggerFactory.getLogger(CommandEventListener.class);
@@ -34,13 +45,32 @@ public class CommandEventListener extends ListenerAdapter {
     }
 
     @Override
-    public void onGenericMessage(@NotNull GenericMessageEvent event) {
-        //TODO: Play
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;
+        Guild guild = event.getGuild();
+
+        Optional<Server> server = SERVER_REPO.getByGuildID(guild.getIdLong());
+        if (server.isEmpty()) {
+            sendMessageToMusicChannel(guild, "We could not find your server in our List. Pls use the command /setup");
+            return;
+        }
+
+        TextChannel musicChannel = guild.getTextChannelById(server.get().getChannelId());
+        if (musicChannel == null) {
+            if (event.getChannelType() == ChannelType.TEXT) {
+                event.getChannel().sendMessage(getMessageData("We could not find your music channel. Pls use the command /setup")).queue();
+            }
+            return;
+        }
+
+        event.getMessage().delete().queueAfter(500, TimeUnit.MILLISECONDS);
+
+        ((PlayCommand) commandRegistry.getList().get("play")).action(event);
     }
 
     @Override
     public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
-        super.onButtonInteraction(event);
+        //TODO: button interaction
     }
 
     @Override
--- src/main/java/com/github/russiaplayer/commands/PlayCommand.java
+++ src/main/java/com/github/russiaplayer/commands/PlayCommand.java
@@ -6,9 +6,11 @@ import com.github.russiaplayer.exceptions.NotFoundException;
 import com.github.russiaplayer.music.PlayerManager;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
@@ -19,6 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.github.russiaplayer.bot.MessageSender.getMessageData;
+import static com.github.russiaplayer.bot.MessageSender.sendMessageToMusicChannel;
 
 public class PlayCommand implements Command {
     private static final ServerRepo SERVER_REPO = ServerRepo.getInstance();
@@ -68,6 +71,53 @@ public class PlayCommand implements Command {
         }
         PlayerManager.getInstance().loadAndPlay(musicChannel, trackUrl);
         event.reply(getMessageData("Now playing the song")).setEphemeral(true).queue();
+    }
+
+    public void action(MessageReceivedEvent event) {
+        Guild guild = event.getGuild();
+
+        GuildVoiceState userVoiceState = event.getMember().getVoiceState();
+        GuildVoiceState botVoiceState = guild.getSelfMember().getVoiceState();
+
+        if (userVoiceState == null || botVoiceState == null) {
+            throw new NotFoundException("VoiceState not found", "Your or the VoiceState of the bot was not found. Pls contact us!");
+        }
+
+        AudioChannelUnion userChannel = event.getMember().getVoiceState().getChannel();
+        AudioChannelUnion botChannel = event.getGuild().getSelfMember().getVoiceState().getChannel();
+
+        if (userChannel == null) {
+            sendMessageToMusicChannel(guild, "You must join a VoiceChannel");
+            return;
+        }
+
+        if (botChannel != null && botChannel != userChannel) {
+            sendMessageToMusicChannel(guild, "You are not in the same VoiceChannel");
+            return;
+        }
+
+        Optional<Server> server = SERVER_REPO.getByGuildID(guild.getIdLong());
+        if (server.isEmpty()) {
+            if (event.getChannelType() == ChannelType.TEXT) {
+                event.getChannel().sendMessage(getMessageData("We could not find your server in our List. Pls use the command /setup")).queue();
+            }
+            return;
+        }
+
+        TextChannel musicChannel = guild.getTextChannelById(server.get().getChannelId());
+        if (musicChannel == null) {
+            if (event.getChannelType() == ChannelType.TEXT) {
+                event.getChannel().sendMessage(getMessageData("We could not find your music channel. Pls use the command /setup")).queue();
+            }
+            return;
+        }
+
+        guild.getAudioManager().openAudioConnection(userChannel);
+        String trackUrl = event.getMessage().getContentRaw();
+        if (!isUrl(trackUrl)) {
+            trackUrl = "ytsearch:" + trackUrl;
+        }
+        PlayerManager.getInstance().loadAndPlay(musicChannel, trackUrl);
     }
 
     private boolean isUrl(String url) {
